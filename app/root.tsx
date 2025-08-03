d85799583564baca5de6b181e63bbf48328a3689
Juan Agustin Zitelli <juanzitelli7@gmail.com>
2025-08-03 20:46:30 -0300

    fix tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,7 @@ import {
   isRouteErrorResponse,
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
-import stylesheet from "~/tailwind.css";
+import stylesheet from "~/tailwind.css?url";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
