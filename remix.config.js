c635d108ceaa1f074277f70c50f5ff8c89819b05
Juan Agustín Zitelli <juanzitelli7@gmail.com>
2025-08-03 20:37:17 -0300

    Actualizar configuración de Remix para Vercel

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,9 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ["**/.*"],
-  tailwind: true,
-  postcss: true,
+  future: {
+    v3_fetcherPersist: true,
+    v3_relativeSplatPath: true,
+    v3_throwAbortReason: true,
+  },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,24 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { vercelPreset } from "@vercel/remix/vite";
 
 installGlobals();
 
 export default defineConfig({
   plugins: [
     remix({
-      presets: [vercelPreset()],
-      ignoredRouteFiles: ["**/.*"],
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
     }),
     tsconfigPaths(),
   ],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: "esnext",
+  },
 });
