ff1ca99f66ad0924fd4c039894b7d217a5fbe29f
Juan Agustin Zitelli <juanzitelli7@gmail.com>
2025-08-03 17:05:23 -0300

    fix vercel deploy 3

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,10 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { vercelPreset } from "@vercel/remix/vite";
+
+installGlobals();
 
 export default defineConfig({
   plugins: [
